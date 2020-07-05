4104106be88b90b766224ae861c390cff6e00d23
Alexandros Kotzias <18498649+akotzias@users.noreply.github.com>
2020-07-05 12:21:58 +0200

    Update config.fish

--- config.fish
+++ config.fish
@@ -1,6 +1,5 @@
 alias showFiles 'defaults write com.apple.finder AppleShowAllFiles YES; killall Finder /System/Library/CoreServices/Finder.app'
 alias hideFiles 'defaults write com.apple.finder AppleShowAllFiles NO; killall Finder /System/Library/CoreServices/Finder.app'
-alias de "eval (docker-machine env default)"
 
 alias dirSizes "du -hs * | sort -nr"
 
@@ -16,8 +15,4 @@ alias ..... 'cd ../../../..'
 alias i "apt-get install"
 alias s "apt-cache search"
 alias h "history | grep"
-alias aria "aria2c -x16 --http-user=transmission --http-passwd=updatethis"
-alias dcup "docker-compose -f ~/repos/wfp/infrastructure/docker/local-environment/docker-compose.yml up"
-alias dcdown "docker-compose -f ~/repos/wfp/infrastructure/docker/local-environment/docker-compose.yml down"
-alias wfpgradle "cd ~/repos/wfp and ./gradlew clean ear --rerun-tasks"
 
